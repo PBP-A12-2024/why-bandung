1dd252a3668f8ab1bf1532e8f32324676cb9200c
maharani.k4rina21@gmail.com <maharani.k4rina21@gmail.com>
2024-10-09 16:54:39 +0700

    add unit test

--- geomapping/views.py
+++ geomapping/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_geomap(request):
+    return render(request, "geomap.html")
--- why_bandung/urls.py
+++ why_bandung/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from geomapping.views import show_geomap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', show_geomap, name='show_geomap'),
 ]
