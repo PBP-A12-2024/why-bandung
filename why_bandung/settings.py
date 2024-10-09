704fd61238e027350f614343db1c76a186c9789b
F00-Fighter <farrel.drmwn00@gmail.com>
2024-10-09 11:04:22 +0700

    django models made

--- why_bandung/settings.py
+++ why_bandung/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
+    'explore',
+    'geomapping',
+    'product_page',
+    'search_system',
+    'why_bandung'
 ]
 
 MIDDLEWARE = [
