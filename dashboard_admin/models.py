3e56cee529d5e1a415dee39bc9aefdd58bcc9cc2
nayjohara <nayajoharar@gmail.com>
2024-10-25 23:52:44 +0700

    fixing edit_product

--- dashboard_admin/models.py
+++ dashboard_admin/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 import uuid
-import os
-
-def get_image_filename(instance, filename):
-    # Ekstrak ekstensi file yang diupload (misalnya .jpg, .png)
-    ext = filename.split('.')[-1]
-    
-    # Format nama file baru berdasarkan id produk
-    filename = f"{instance.id}.{ext}"
-    
-    # Tentukan folder penyimpanan gambar
-    return os.path.join('product_images/', filename)
 
 # Create your models here.
 class TokoEntry(models.Model):
@@ -27,5 +16,5 @@ class ProductEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField(validators=[MinValueValidator(1)])
     description = models.TextField()
-    image = models.ImageField(upload_to=get_image_filename, blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     toko = models.ForeignKey(TokoEntry, on_delete=models.CASCADE, related_name='products')
