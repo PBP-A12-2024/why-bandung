34b95153746ac6808bbb730a3f576a4a6441e74a
FadhilNurA <afadhil15156@gmail.com>
2024-10-27 19:56:46 +0700

    menyelesaikan test

--- product_page/tests.py
+++ product_page/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_nonexistent_page(self):
+        response = Client().get('/sadcat/')
+        self.assertEqual(response.status_code, 404)
+        
--- whatToEat/tests.py
+++ whatToEat/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_nonexistent_page(self):
+        response = Client().get('/sadcat/')
+        self.assertEqual(response.status_code, 404)
